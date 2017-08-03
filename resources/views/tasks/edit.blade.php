50db13dffd250d1feb7e87bc4a58385e09cbd4f2
Maiko Ishikawa <mishikawa@revee.jp>
2017-08-03 14:32:21 +0000

    Procfile

--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -4,16 +4,20 @@
 
     <h1>id: {{ $task->id }} のタスク編集ページ</h1>
 
-    {!! Form::model($task, ['route' => ['tasks.update', $task->id], 'method' => 'put']) !!}
-
-        {!! Form::label('status', 'ステータス:') !!}
-        {!! Form::text('status') !!}
-
-        {!! Form::label('content', 'メッセージ:') !!}
-        {!! Form::text('content') !!}
-
-        {!! Form::submit('更新') !!}
-
-    {!! Form::close() !!}
+    <div class="row">
+        <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-lg-6 col-lg-offset-3">
+            {!! Form::model($task, ['route' => ['tasks.update', $task->id], 'method' => 'put']) !!}
+        
+                {!! Form::label('status', 'ステータス:') !!}
+                {!! Form::text('status', null, ['class' => 'form-control']) !!}
+        
+                {!! Form::label('content', 'メッセージ:') !!}
+                {!! Form::text('content', null, ['class' => 'form-control']) !!}
+        
+                {!! Form::submit('更新', ['class' => 'btn btn-primary']) !!}
+        
+            {!! Form::close() !!}
+        </div>
+    </div>
 
 @endsection
